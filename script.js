459cdce689d91fcf7ada94ac553091e657c0daa1
Ongky <ongky@email.com>
2025-07-14 09:57:57 +0700

    Update

--- script.js
+++ script.js
@@ -3,19 +3,6 @@ function showSection(id) {
   document.getElementById(id).classList.add("active");
 }
 
-function logout() {
-  alert("Logout berhasil!");
-  // nanti redirect login admin
-}
-
-function tambahTruk() {
-  alert("Tambah data truk (fitur segera dibuat)");
-}
-
-function tambahSupir() {
-  alert("Tambah data supir (fitur segera dibuat)");
-}
-
 function tambahSparepart() {
   const nama = prompt("Nama Sparepart:");
   const harga = prompt("Harga (Rp):");
@@ -62,14 +49,6 @@ function hapusSparepart(index) {
 
 // Panggil saat awal buka
 tampilkanSparepart();
-
-function tambahPengeluaran() {
-  alert("Tambah data pengeluaran (fitur segera dibuat)");
-}
-
-function tambahServis() {
-  alert("Tambah jadwal servis (fitur segera dibuat)");
-}
 
 let formMode = '';
 let formType = '';
